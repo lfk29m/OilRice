766e3bef6fdbdf25abb03225547fb905dfdaecff
lfk29m12 <lfk29m12@yahoo.com.tw>
2025-01-28 11:28:56 +0800

    加上總價、reset按鈕

--- src/components/MenuTable.vue
+++ src/components/MenuTable.vue
@@ -9,6 +9,21 @@ const appStore = useAppStore()
 
 const menuItem = computed(() => appStore.menus.find((item) => item.id === props.id))
 
+/**
+ * 總價
+ */
+const totalPrice = computed(() => {
+  if (!menuItem.value) return 0
+  return menuItem.value.items.reduce((acc, item) => acc + item.price * (item.count ?? 0), 0)
+})
+
+const reset = () => {
+  if (!menuItem.value) return
+  menuItem.value.items.forEach((item) => {
+    item.count = null
+  })
+}
+
 const removeMenuItem = () => {
   appStore.menus = appStore.menus.filter((item) => item.id !== props.id)
 }
@@ -16,7 +31,10 @@ const removeMenuItem = () => {
 
 <template>
   <section v-if="menuItem" class="menuTable">
-    <div class="menuTable__removeBtn" @click="removeMenuItem">X</div>
+    <div class="menuTable__controller">
+      <button @click="reset">reset</button>
+      <div class="menuTable__removeBtn" @click="removeMenuItem">X</div>
+    </div>
     <table>
       <thead>
         <tr>
@@ -33,6 +51,8 @@ const removeMenuItem = () => {
         </tr>
       </tbody>
     </table>
+    <hr />
+    <div class="totalPrice">總價: {{ totalPrice }}</div>
   </section>
 </template>
 
@@ -42,10 +62,13 @@ const removeMenuItem = () => {
   border: 1px solid #ccc;
   padding: 1rem;
 
+  &__controller {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+
   &__removeBtn {
-    position: absolute;
-    top: 0;
-    right: 0;
     padding: 0.5rem;
     cursor: pointer;
     color: #ff0000;
@@ -77,6 +100,16 @@ const removeMenuItem = () => {
     input {
       width: 100%;
     }
+  }
+
+  hr {
+    margin: 1rem 0;
+    border-color: #e9e9e93d;
+  }
+
+  .totalPrice {
+    font-size: 1.5rem;
+    text-align: right;
   }
 }
 </style>
