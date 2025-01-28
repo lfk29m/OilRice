b445d59b605dfeaf3640fea83ea9c2f5a626e7e6
lfk29m12 <lfk29m12@yahoo.com.tw>
2025-01-28 11:50:50 +0800

    補上註解

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/OilRice/',
+  base: '/OilRice/', // 參考: https://vite.dev/guide/static-deploy.html#github-pages
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
